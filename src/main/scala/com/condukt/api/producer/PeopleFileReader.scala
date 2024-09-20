638a615da7bca24d96eaa2ac6ba499e755369b50
maniacviper <maniacviper@gmail.com>
2024-09-20 23:20:48 +0100

    handling errors when reading the json file

--- src/main/scala/com/condukt/api/producer/PeopleFileReader.scala
+++ src/main/scala/com/condukt/api/producer/PeopleFileReader.scala
@@ -7,13 +7,18 @@ import com.condukt.api.producer.model.{Person, RandomPeople}
 import io.circe.generic.auto._
 import io.circe.parser.parse
 import io.circe.{DecodingFailure, Json}
-
+import java.nio.file.NoSuchFileException
 import scala.io.Source
 
 object PeopleFileReader {
   def apply[F[_] : Sync](filePath: String): F[List[Person]] = {
-    val resourceStream = getClass.getClassLoader.getResourceAsStream(filePath)
-    val fileContent = Resource.make(Sync[F].delay(Source.fromInputStream(resourceStream)))(source => Sync[F].delay(source.close()))
+    val maybeResourceStream = Option(getClass.getClassLoader.getResourceAsStream(filePath))
+    val fileContent = maybeResourceStream match {
+      case Some(resourceStream) =>
+        Resource.make(Sync[F].delay(Source.fromInputStream(resourceStream)))(source => Sync[F].delay(source.close()))
+      case None =>
+        Resource.eval(Sync[F].raiseError(new NoSuchFileException(s"$filePath not found")))
+    }
     val json: F[Json] = fileContent.use(source => Sync[F].delay(parse(source.getLines().mkString).getOrElse(Json.Null)))
     val decodePeople: F[Either[DecodingFailure, List[Person]]] = json.map(_.as[RandomPeople].map(_.ctRoot))
     decodePeople.flatMap {
--- src/test/scala/com/condukt/api/producer/PeopleFileReaderSpec.scala
+++ src/test/scala/com/condukt/api/producer/PeopleFileReaderSpec.scala
@@ -2,8 +2,11 @@ package com.condukt.api.producer
 
 import cats.effect.IO
 import cats.effect.unsafe.implicits.global
+import io.circe.DecodingFailure
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.wordspec.AnyWordSpec
+
+import java.nio.file.NoSuchFileException
 
 class PeopleFileReaderSpec extends AnyWordSpec with Matchers {
 
@@ -12,6 +15,21 @@ class PeopleFileReaderSpec extends AnyWordSpec with Matchers {
       "file exists and has correct json structure" in {
         val result = PeopleFileReader[IO]("test-random-people-data.json").unsafeRunSync()
         result.size mustBe 2
+      }
+    }
+
+    "return error" when {
+      "no file exists" in {
+        val error = intercept[NoSuchFileException] {
+          PeopleFileReader[IO]("invalid-file.json").unsafeRunSync()
+        }
+        error.getMessage mustBe s"invalid-file.json not found"
+      }
+      "file exists but is not the correct json structure" in {
+        val error = intercept[DecodingFailure] {
+          PeopleFileReader[IO]("invalid-random-people-data.json").unsafeRunSync()
+        }
+        error.getMessage mustBe s"DecodingFailure at .ctRoot[0].address.postode: Missing required field"
       }
     }
   }
